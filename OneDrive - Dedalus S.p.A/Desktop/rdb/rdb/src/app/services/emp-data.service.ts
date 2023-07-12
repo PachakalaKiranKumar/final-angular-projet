6d93e76941346addd6e3281e0dfb8f03f61b60e6
PachakalaKiranKumar <kiranp5477@gmail.com>
2023-07-12 10:11:10 +0530

    last commit

---
+++ OneDrive - Dedalus S.p.A/Desktop/rdb/rdb/src/app/services/emp-data.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { server } from './allservers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpDataService {
+  private empId: string = "";
+
+  constructor(private http: HttpClient, private serverService:server) {}
+
+  getEmployeeById(employeeId: string): Observable<any> {
+    const apiUrl = this.serverService.ServerUrls +'employee/' + employeeId;
+    return this.http.get(apiUrl);
+  }
+
+  getId(): string {
+    return this.empId;
+  }
+
+  setId(empId: string): void {
+    this.empId = empId;
+  }
+}
